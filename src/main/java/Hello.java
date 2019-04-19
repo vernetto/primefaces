38657902e1ad2cab0da52de2f372452b8949e328
vernetto <publicpierre@gmail.com>
2019-04-19 22:33:16 +0200

    first commit

---
+++ src/main/java/Hello.java
@@ -0,0 +1,16 @@
+import javax.inject.Named;
+import javax.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+@Named(value="beppe")
+public class Hello {
+    String name = "Pierre";
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
